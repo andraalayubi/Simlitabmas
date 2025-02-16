189cb2272c2a67ddf9255201660b5c268f1fedbc
mirza27 <ramadhanimirza10@gmail.com>
2025-02-16 15:24:25 +0700

    fix(Migration): make column year_research, rg, department unique

---
+++ prisma/migrations/20250216081122_make_unique_column_name_for_table_year_rg_and_department/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `departments` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[name]` on the table `research_groups` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[year]` on the table `year_researches` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "departments_name_key" ON "departments"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "research_groups_name_key" ON "research_groups"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "year_researches_year_key" ON "year_researches"("year");
