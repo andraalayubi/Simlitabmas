1e557677659507b4e74dcaf97e24348f2b507025
mirza27 <ramadhanimirza10@gmail.com>
2025-02-17 15:21:33 +0700

    fix(Schema): fix page for admin audit

---
+++ src/app/(pages)/audit/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useParams, usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+// redirecting page only
+
+export default function AuditDetailPage() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const params = useParams();
+
+  // redirect to tab 'overview'
+  useEffect(() => {
+    if (!pathname.includes("skema")) {
+      router.replace(`/audit/skema`);
+    }
+  }, [pathname]);
+
+  return null;
+}
--- src/app/(pages)/audit/skema/page.tsx
+++ src/app/(pages)/audit/skema/page.tsx
@@ -1,30 +1,64 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import MainLayout from 'src/components/layouts/MainLayout';
-import DaftarSkema from 'src/components/audit/Skema';
-import { Skema } from '@/app/types/Interfaces';
-import LoadingPage from 'src/components/usulan/LoadingPage';
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Text } from "@mantine/core";
+import DaftarSkema from "src/components/audit/Skema";
+import LoadingPage from "src/components/usulan/LoadingPage";
+import { schema } from "prisma/interfaces";
+import { MRT_ColumnDef } from "mantine-react-table";
+import useNotification from "src/components/notification/notification";
+import TableLayout from "src/components/table/tableLayout";
+import schemaAction from "src/action/schemaAction";
+import ModalComponent from "src/components/modal/modal";
 
-const SkemaPage: React.FC = () => {
-  const [skemaList, setSkemaList] = useState<Skema[]>([]);
+export default function AuditSchemaPage() {
+  const user_type = "admin";
+  const [schema, setSchema] = useState<schema[]>([]);
   const [loading, setLoading] = useState(true);
+  const { showNotification } = useNotification();
+
+  const columns = React.useMemo<MRT_ColumnDef<schema>[]>(
+    () => [
+      {
+        accessorKey: "id",
+        header: "No",
+        size: 50,
+      },
+      {
+        accessorKey: "name",
+        header: "Nama Skema",
+        size: 150,
+      },
+      {
+        accessorKey: "description",
+        header: "Deskripsi Skema",
+        size: 300,
+      },
+      {
+        accessorKey: "proposal_suggestion_count",
+        header: "Jumlah Usulan",
+        size: 100,
+      },
+    ],
+    []
+  );
+
+  // get schema list
+  const getSchema = useCallback(async () => {
+    const response = await schemaAction.getSchemas(user_type, setLoading);
+
+    console.log(response.data);
+    if (response.success) {
+      setSchema(response.data);
+      showNotification({ status: "success", message: response.message });
+    } else {
+      showNotification({ status: "error", message: response.message });
+    }
+  }, [user_type]);
 
   useEffect(() => {
-    const fetchSkemaData = async () => {
-      try {
-        const response = await axios.get('/api/skema');
-        setSkemaList(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching skema data:', error);
-        setLoading(false);
-      }
-    };
-
-    fetchSkemaData();
-  }, []);
+    getSchema();
+  }, [getSchema]);
 
   if (loading) {
     return <LoadingPage />;
@@ -32,12 +66,29 @@ const SkemaPage: React.FC = () => {
 
   return (
     <>
-      <nav className="text-sm text-gray-600 mb-4">
-        Audit {'>'} Skema
-      </nav>
-      <DaftarSkema skemaList={skemaList} />
+      <div className="container mx-auto px-4 py-6">
+        <div className="bg-white shadow rounded-lg mt-4">
+          <div className="bg-white shadow sm:rounded-lg">
+            {/* Judul */}
+            <div className="flex justify-between items-center pt-5 pb-2 px-6">
+              <Text size="lg" fw={700}>
+                Daftar Skema Usulan
+              </Text>
+              <ModalComponent title="Buat Skema">
+                {(close) => <> </>}
+              </ModalComponent>
+            </div>
+
+            <TableLayout
+              columns={columns}
+              data={schema}
+              isLoading={loading}
+              enableRowClick={true}
+              getRowClickUrl={(row) => `/schema/${row.id}`}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
-};
-
-export default SkemaPage;
+}
