af84a10e4364b2b05e32ec8f45adb56280426939
mirza27 <ramadhanimirza10@gmail.com>
2025-02-17 15:04:58 +0700

    fix(Notification): fix overwrite mantine css style

--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -5,7 +5,9 @@ import {
   IconInfoCircle,
   IconX,
 } from "@tabler/icons-react";
-import "@mantine/notifications/styles.css";
+import { useMantineTheme } from "@mantine/core";
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 
 type NotificationStatus = "error" | "success" | "warning" | "info";
 
@@ -17,36 +19,61 @@ interface NotificationProps {
 }
 
 const useNotification = () => {
+  const theme = useMantineTheme();
+
+  const getNotificationColor = (status: NotificationStatus) => {
+    const colors = {
+      success: theme.colors.teal[6],
+      error: theme.colors.red[6],
+      warning: theme.colors.yellow[6],
+      info: theme.colors.blue[6],
+    };
+    return colors[status];
+  };
+
+  const getNotificationIcon = (status: NotificationStatus) => {
+    const iconProps = {
+      size: "1.1rem",
+      // color: getNotificationColor(status),
+    };
+
+    switch (status) {
+      case "success":
+        return <IconCheck {...iconProps} />;
+      case "error":
+        return <IconX {...iconProps} />;
+      case "warning":
+        return <IconAlertTriangle {...iconProps} />;
+      case "info":
+        return <IconInfoCircle {...iconProps} />;
+      default:
+        return null;
+    }
+  };
+
   const showNotification = ({
     status,
     message,
     isLoading = false,
     duration = 3000,
   }: NotificationProps) => {
-    const getNotificationIcon = (status: NotificationStatus) => {
-      switch (status) {
-        case "success":
-          return <IconCheck size={18} />;
-        case "error":
-          return <IconX size={18} />;
-        case "warning":
-          return <IconAlertTriangle size={18} />;
-        case "info":
-          return <IconInfoCircle size={18} />;
-        default:
-          return null;
-      }
-    };
-
     notifications.show({
       id: `notification-${status}`,
       title: status.charAt(0).toUpperCase() + status.slice(1),
       message,
       loading: isLoading,
-      autoClose: duration,
-      color: status,
+      autoClose: isLoading ? false : duration,
+      // color: getNotificationColor(status),
       icon: getNotificationIcon(status),
+      withCloseButton: true,
+      withBorder: true,
       position: "top-right",
+      styles: (theme) => ({
+        icon: {
+          backgroundColor: "transparent !important",
+          marginRight: theme.spacing.md,
+        },
+      }),
     });
   };
 
