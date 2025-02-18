1c1e7400a3f77ec9cf56108e9c5946e00d5eab68
mirza27 <ramadhanimirza10@gmail.com>
2025-02-18 12:30:05 +0700

    fix(Year Research): create page for admin audit

--- src/app/(pages)/audit/tahun/page.tsx
+++ src/app/(pages)/audit/tahun/page.tsx
@@ -1,66 +1,21 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Tabs } from '@mantine/core';
-import MainLayout from 'src/components/layouts/MainLayout';
-import DaftarTahun from 'src/components/audit/ListTahun';
-import RekapProgress from 'src/components/audit/RekapProgress';
-import LoadingPage from 'src/components/usulan/LoadingPage';
+import { useParams, usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
-const TahunPage: React.FC = () => {
-  const [tahunList, setTahunList] = useState([]);
-  const [usulans, setUsulans] = useState([]);
-  const [loading, setLoading] = useState(true);
+// redirecting page only
 
+export default function AuditTahunDetailPage() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const params = useParams();
+
+  // redirect to tab 'overview'
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const responseTahun = await axios.get('/api/tahun');
-        const responseUsulans = await axios.get('/api/rekap_progress');
-        setTahunList(responseTahun.data);
-        setUsulans(responseUsulans.data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      }
-    };
+    if (!pathname.includes("tahun_usulan")) {
+      router.replace(`/audit/tahun/tahun_usulan`);
+    }
+  }, [pathname]);
 
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <LoadingPage />;
-  }
-
-  return (
-    <>
-      <nav className="text-sm text-gray-600 mb-4">
-        Audit {'>'} Tahun
-      </nav>
-      <div className='max-w-screen'>
-      <Tabs color="rgba(19, 41, 99, 1)" defaultValue="tahun" radius="md" keepMounted={false}>
-        <Tabs.List grow className="flex flex-wrap">
-          <Tabs.Tab value="tahun" className="whitespace-nowrap">Tahun</Tabs.Tab>
-          <Tabs.Tab value="rekapProgress" className="whitespace-nowrap">Rekap Progress</Tabs.Tab>
-        </Tabs.List>
-
-        <Tabs.Panel value="tahun" pt="xs">
-          <div className="overflow-x-auto">
-            <DaftarTahun tahunList={tahunList} />
-          </div>
-        </Tabs.Panel>
-
-        <Tabs.Panel value="rekapProgress" pt="xs">
-          <div className="overflow-x-auto">
-            <RekapProgress usulans={usulans} />
-          </div>
-        </Tabs.Panel>
-      </Tabs>
-      </div>
-    </>
-  );
-};
-
-export default TahunPage;
+  return null;
+}
