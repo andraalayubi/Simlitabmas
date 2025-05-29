fabaeb918e1dd464138843b510c329a2d11fa04a
mirza27 <ramadhanimirza10@gmail.com>
2025-05-29 07:41:54 +0700

    fix(Create Proposal Suggestion): fix filter schema by position_id

--- src/app/api/lecturer/(schema)/schema/route.ts
+++ src/app/api/lecturer/(schema)/schema/route.ts
@@ -43,18 +43,19 @@ export async function GET(request: NextRequest) {
     let requiredPositionId = null
     if (filter.position_id != null) {
       requiredPositionId = filter.position_id
-      
+
       delete filter.position_id;
     }
 
     // Panggil service dengan filter yang sudah dimodifikasi
-    let schemas = await schemaService.getByFilter(filter);
+    let schemas = await schemaService.getByFilter(filter, { position_schema: true });
+
 
     if (requiredPositionId !== null) {
       schemas = schemas.filter((schema: any) => {
         // Pastikan position_schemas terload dan tidak null
         if (!schema.position_schema) return false;
-        
+
         // Cek apakah ada position schema yang match
         return schema.position_schema.some(
           (ps: any) => ps.position_id === requiredPositionId
