10abf76b99558744d1445271ac922f76b4afb837
andraalayubi <andraalayubi@gmail.com>
2025-06-17 15:44:22 +0700

    fix(anggota): fixing api ketua rg

--- src/app/api/ketua_rg/(proposal_suggestion)/member/[proposal_suggestion_id]/route.ts
+++ src/app/api/ketua_rg/(proposal_suggestion)/member/[proposal_suggestion_id]/route.ts
@@ -1,16 +1,13 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { NextRequest, NextResponse } from "next/server";
 import lecturerService from "src/services/lecturerService";
-import schemaService from "src/services/schemaService";
+import memberService from "src/services/memberService";
 
 export async function GET(req: NextRequest, { params }: { params: Params }) {
     try {
         const proposalSuggestionId = parseInt(params.proposal_suggestion_id, 10);
-
-        const body = await req.json();
-
-        const result = await lecturerService.addLecturerMember(proposalSuggestionId, body.lecturerId);
-
+        const proposal_suggestion = await memberService.getLecturerMembers(proposalSuggestionId);
+        
         return NextResponse.json({
             success: true,
             message: "Success getting data",
@@ -25,21 +22,25 @@ export async function GET(req: NextRequest, { params }: { params: Params }) {
     }
 }
 
-export async function GET(req: NextRequest, { params }: { params: Params }) {
-    try {
+export async function POST(req: NextRequest, { params }: { params: Params }) {
+    try {      
         const proposalSuggestionId = parseInt(params.proposal_suggestion_id, 10);
-        const proposal_suggestion = await schemaService.getByProposalSuggestionId(proposalSuggestionId);
+
+        const body = await req.json();
         
+        const result = await lecturerService.addLecturerMember(proposalSuggestionId, body.anggota);
+
         return NextResponse.json({
             success: true,
-            message: "Success getting data",
-            data: proposal_suggestion
-        }, { status: 200 })
+            data: result,
+            message: "Lecturer associated with proposal suggestion successfully"
+        }, { status: 201 });
 
     } catch (error: any) {
+        console.error("Error creating proposal suggestion:", error);
         return NextResponse.json({
             success: false,
-            message: `Internal Server error: ${error.message}`
+            message: `Internal Server Error: ${error.message}`,
         }, { status: 500 });
     }
 }
